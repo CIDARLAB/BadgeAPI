08fb08105d7d02b24048aba7a19ba2b5648dc3bd
David Tran <djtran@bu.edu>
2016-06-29 14:52:31 -0400

    trying to setup mongo in docker

--- FinalProjectJoseph/Code/setupMongoDB.sh
+++ FinalProjectJoseph/Code/setupMongoDB.sh
@@ -3,12 +3,14 @@
 sudo su
 echo "If this script doesn't run you don't have mongodb properly setup"
 
-mongo pcrhero
-echo "db.createCollection(users)" | mongo pcrhero
-echo "db.createCollection(badges)" | mongo pcrhero
-echo "db.createCollection(issuers)" | mongo pcrhero 
-echo "db.createCollection(users)"| mongo pcrhero
-echo "db.createCollection(tasks)" | mongo pcrhero
+mongo
+use pcrhero
+db.createCollection('apps')
+db.createCollection('badges')
+db.createCollection('issuers')
+db.createCollection('users')
+db.createCollection('tasks')
+exit
 
 echo "Now make sure you've modified the IP in pcrserver.py and pcrlib.py. Also change the links in base.tpl so they don't travel outside to the actual pcrhero url"
 echo "Make sure that mongo's db path is set and writable, and then setup your filesystem folders to store badge related info"
