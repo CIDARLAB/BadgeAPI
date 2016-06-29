af24c40ce87bd8a6d139e58ab5913a8f81c17765
David Tran <djtran@bu.edu>
2016-06-29 13:19:11 -0400

    Dockering pls

--- FinalProjectJoseph/Code/setupMongoDB.sh
+++ FinalProjectJoseph/Code/setupMongoDB.sh
@@ -3,12 +3,12 @@
 sudo su
 echo "If this script doesn't run you don't have mongodb properly setup"
 
-mongo "use pcrhero"
-mongo "db.createCollection(users)"
-mongo "db.createCollection(badges)"
-mongo "db.createCollection(issuers)"
-mongo "db.createCollection(users)"
-mongo "db.createCollection(tasks)"
+mongo pcrhero
+echo "db.createCollection(users)" | mongo pcrhero
+echo "db.createCollection(badges)" | mongo pcrhero
+echo "db.createCollection(issuers)" | mongo pcrhero 
+echo "db.createCollection(users)"| mongo pcrhero
+echo "db.createCollection(tasks)" | mongo pcrhero
 
 echo "Now make sure you've modified the IP in pcrserver.py and pcrlib.py. Also change the links in base.tpl so they don't travel outside to the actual pcrhero url"
 echo "Make sure that mongo's db path is set and writable, and then setup your filesystem folders to store badge related info"
