2440f31989265d2b184c3df3e51514398cd6bc91
David Tran <djtran@bu.edu>
2016-07-14 14:03:36 -0400

    updated

--- FinalProjectJoseph/Code/pcrserver.py
+++ FinalProjectJoseph/Code/pcrserver.py
@@ -70,7 +70,7 @@ def bakedawarded(filename):
 def userbadges():
     email = request.params.username
 
-    return get_users_badges(db,email)
+    return pcrlib.get_users_badges(db,email)
     ##Get request for JSON of user badges.
 
 @post('/search')
